b709baf941390408166ab4ba1814c034fffda88a
gordon1991112000 <gordon19911172000@gmail.com>
2022-05-27 13:54:16 +0800

    volume mount change to release folder instead of current folder

--- docker_swarm/compose/deploy.sh
+++ docker_swarm/compose/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cd ../../ipx/releases/
+latest_release=`ls -td -- */ | head -n 1`
+cd ../../ecdock/compose
+sed -i "/PROJECT_ROOT/s|releases.*|releases\/${latest_release}|" ".env.ipx"
 set -a && . .env.ipx && set +a && docker stack deploy --compose-file ipx.yml ecdock
